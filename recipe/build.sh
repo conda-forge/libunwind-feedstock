9a13e1a6b0db106ee2ca963b8b2989fbf7fd6c1a
Uwe L. Korn <uwe.korn@quantco.com>
2020-09-04 10:52:04 +0200

    Disable static build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --disable-static
 # First call only the install target as compilation for tests only works with an installed libunwind
 make install -j${CPU_COUNT}
 make -j${CPU_COUNT}
