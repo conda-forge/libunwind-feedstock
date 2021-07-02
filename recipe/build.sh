f54624cbff441fb7a73d617b1bd67a3d70e2ba91
Matthias Diener <mdiener@illinois.edu>
2021-07-02 11:43:22 -0700

    simplify makefile

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,7 @@ autoreconf -fiv
 
 ./configure --prefix=$PREFIX --disable-static
 
-# First call only the install target as compilation for tests only works with an installed libunwind
-make install -j${CPU_COUNT}
 make -j${CPU_COUNT}
-
 
 # The tests are known to be flakey so disable them
 # make check || true
