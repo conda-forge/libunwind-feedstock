38e7322c4493cefa351e735cabac0864be013dca
Uwe Korn <uwelk@xhochy.com>
2025-07-11 17:41:37 +0000

    Disable tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ autoreconf -fiv
 # Source: https://llvm.org/doxygen/BinaryFormat_2ELF_8h_source.html
 sed -i '1s;^;#define NT_ARM_PAC_MASK 0x406;' src/ptrace/_UPT_ptrauth_insn_mask.c
 
-./configure --prefix=$PREFIX --disable-static
+./configure --prefix=$PREFIX --disable-static --disable-tests
 
 make -j${CPU_COUNT}
 make install
